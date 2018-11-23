382bba02c55dc011d1a91abf79658ec410f442ac
talitm555 <Talitm555@gmail.com>
2018-11-23 23:16:46 +0500

    FrontEnd

---
+++ CampusSocialNetwork-master/Campus Social Network/Controllers/AdminController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Campus_Social_Network.Controllers
+{
+    public class AdminController : Controller
+    {
+        
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddStudent()
+        {
+            return View();
+        }
+
+        public ActionResult AddTeacher()
+        {
+            return View();
+        }
+
+        public ActionResult AddClass()
+        {
+            return View();
+        }
+
+        public ActionResult AllStudents()
+        {
+            return View();
+        }
+
+        public ActionResult AllTeachers()
+        {
+            return View();
+        }
+
+        public ActionResult AllClasses()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateProfile()
+        {
+            return View();
+        }
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+        public ActionResult ChangePicture()
+        {
+            return View();
+        }
+
+
+
+
+
+    }
+}
