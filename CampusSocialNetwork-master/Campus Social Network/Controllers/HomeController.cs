78828fe547c76d69778ffd286a8c0b1b0586680b
AhmedAbdulKhaliq <ahmed106ak@gmail.com>
2018-11-24 00:46:44 +0500

    Admin Login.

--- CampusSocialNetwork-master/Campus Social Network/Controllers/HomeController.cs
+++ CampusSocialNetwork-master/Campus Social Network/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Campus_Social_Network.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace Campus_Social_Network.Controllers
 {
     public class HomeController : Controller
     {
+        private CSNDBEntities entity = new CSNDBEntities();
         public ActionResult Index()
         {
             return View();
@@ -24,6 +26,37 @@ namespace Campus_Social_Network.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel item)
+        {
+            var UserName = "abc";
+            var Password = "abc";
+            List<Admin> admin_temp_lst = new List<Admin>();
+            admin_temp_lst = entity.Admins.ToList();
+            List<AdminPassword> admin_password_temp_list = new List<AdminPassword>();
+            admin_password_temp_list = entity.AdminPasswords.ToList();
+            foreach (Admin obj in admin_temp_lst)
+            {
+                UserName = obj.Email;
+                break;
+            }
+            foreach(AdminPassword obj in admin_password_temp_list)
+            {
+                Password = obj.Password;
+                break;
+            }
+            if(UserName == item.Email && Password == item.Password)
+            {
+                return RedirectToAction("Index", "Admin");
+            }
             return View();
         }
     }
